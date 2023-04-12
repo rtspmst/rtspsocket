a8e3df2300877cb6ebb2c0b6d94dd13a90e56fff
mashaoting <1291203886@qq.com>
2023-04-12 15:49:21 +0800

    cccc

--- app/src/main/java/com/whmst/socketlib/MainActivity.java
+++ app/src/main/java/com/whmst/socketlib/MainActivity.java
@@ -11,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        EasyPlayerClient
     }
 }
