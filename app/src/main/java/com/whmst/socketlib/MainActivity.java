867cda51e980b5cf486cdfa8792939fdad8f850c
mashaoting <1291203886@qq.com>
2023-04-19 11:15:01 +0800

    4444444444

--- app/src/main/java/com/whmst/socketlib/MainActivity.java
+++ app/src/main/java/com/whmst/socketlib/MainActivity.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import cn.jzvd.Jzvd;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Jzvd myJzvdStd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
