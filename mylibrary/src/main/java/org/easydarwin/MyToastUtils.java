dbb1c33d79e2eef00633df5109d6448506a30081
mashaoting <1291203886@qq.com>
2023-07-13 17:00:28 +0800

    888

--- mylibrary/src/main/java/org/easydarwin/MyToastUtils.java
+++ mylibrary/src/main/java/org/easydarwin/MyToastUtils.java
@@ -1,14 +1,14 @@
 package org.easydarwin;
 
 
-//import com.blankj.utilcode.util.ToastUtils;
+import com.blankj.utilcode.util.ToastUtils;
 
 public class MyToastUtils {
 
     public static boolean isShow = true;
 
     public static void show(String info) {
-//        ToastUtils.showShort(info);
+        ToastUtils.showShort(info);
     }
 
     public static void showToast(int i, String toastInfo) {
@@ -19,18 +19,20 @@ public class MyToastUtils {
 
                 case CV.TOAST_TAG1:
 
-//                    ToastUtils.setGravity(1, 1, -200);
+                    ToastUtils.setGravity(1, 1, -200);
 
                     break;
                 case CV.TOAST_TAG2:
 
-//                    ToastUtils.setGravity(1, 1, -100);
+                    ToastUtils.setGravity(1, 1, -100);
 
                     break;
                 case CV.TOAST_TAG3:
 
-//                    ToastUtils.setGravity(1, 1, 0);
+                    ToastUtils.setGravity(1, 1, 0);
 
+                    break;
+                default:
                     break;
             }
 //            ToastUtils.showShort(toastInfo);
