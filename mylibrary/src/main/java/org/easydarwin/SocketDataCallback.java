077c785b9651da29a930e41dcdcfef6bebcb2883
mashaoting <1291203886@qq.com>
2023-08-11 15:52:40 +0800

    000

--- mylibrary/src/main/java/org/easydarwin/SocketDataCallback.java
+++
@@ -1,6 +0,0 @@
-package org.easydarwin;
-
-public interface SocketDataCallback {
-    //接收数据
-    void onReceiveData(byte[] data);
-}
--- mylibrary/src/main/java/org/easydarwin/SocketStateCallback.java
+++
@@ -1,7 +0,0 @@
-package org.easydarwin;
-
-public interface SocketStateCallback {
-
-    //Socket连接状态
-    void onSocketState(byte state);
-}
